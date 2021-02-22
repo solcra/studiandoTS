3a734e1922f1e55a5c2a3e1a397feb24cda7d32b
Solcra <carlosgranadacra@gmail.com>
2021-02-22 08:41:27 -0500

    Terminada el curso de ts

---
+++ photo-app/webpack.config.js
@@ -0,0 +1,22 @@
+const { Module } = require("webpack");
+
+module.exports = {
+    mode: 'production',
+    entry: './src/main.ts',
+    devtool: 'inline-source-map',
+    module:{
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+    }
+}
